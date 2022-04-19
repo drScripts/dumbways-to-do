56c201d5fcceb3c89d1d2e4f0ff59f0e37e7a94b
Nathanael <nathanael.vd@gmail.com>
2022-04-19 23:50:52 +0700

    Add show project

---
+++ controllers/projects/show.js
@@ -0,0 +1,32 @@
+const { Project } = require("../../models");
+const { request, response } = require("express");
+
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ */
+module.exports = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const project = await Project.findByPk(id);
+
+    if (!project)
+      return res.status(404).json({
+        status: "not found",
+        message: "Can't find project with that id",
+      });
+
+    res.status(200).json({
+      status: "created",
+      data: { project },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+};
--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { add, update, destroy, get } = require("../controllers/projects");
+const { add, update, destroy, get, show } = require("../controllers/projects");
 
 router.get("/", get);
+router.get("/:id", show);
 router.post("/", add);
 router.patch("/:id", update);
 router.delete("/:id", destroy);
