b817fa32b48a4c8478b046d8119a230c269d30ad
Nathanael <nathanael.vd@gmail.com>
2022-04-21 19:15:35 +0700

    change project on show

--- controllers/projects/show.js
+++ controllers/projects/show.js
@@ -18,6 +18,12 @@ module.exports = async (req, res) => {
         message: "Can't find project with that id",
       });
 
+    project.dataValues.count_task = project?.tasks?.length;
+    const tasksFinished = project?.tasks?.filter(
+      (task) => task?.status === "complete"
+    );
+    project.dataValues.count_task_complete = tasksFinished?.length || 0;
+
     res.status(200).json({
       status: "created",
       data: { project },
